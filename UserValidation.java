8bab2e8234f8fcea17b8a947c1dca91cc9b6102c
Ishani9 <57087410+Ishani9@users.noreply.github.com>
2020-10-02 21:15:39 +0530

    Added: Functionality to validate user entry

--- UserValidation.java
+++ UserValidation.java
@@ -5,6 +5,31 @@ import java.util.regex.Matcher;
 
 public class UserValidation {
 	
+	//UC10
+	
+	public String validateUser(String name, String surname, String mobile, String email, String password) {
+		if(matchpattern("(^[A-Z]{1}[a-z]{2,}$)",name)&&matchpattern("(^[A-Z]{1}[a-z]{2,}$)", surname)&&matchpattern("^([0-9]{1,3}[ ][0-9]{10})$", mobile)&&matchpattern("^[a-zA-Z0-9]+[-+.]?+[A-Za-z0-9]+[@]+[A-Za-z0-9]+[.][a-z]{2,}+[.]?+([a-z]{2,})?$", email)&&matchpattern("^((?=.*[A-Z]+)(?=.*[0-9]+)([A-Za-z0-9@#$%^&*]{8,}))$", password)) {
+			return "HAPPY";
+		}
+		
+		else {
+			return "SAD";
+		}
+	}
+	
+	public boolean matchpattern(String regex, String input) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher= pattern.matcher(input);
+		if(matcher.find()) {
+			return true;
+		}
+		else {
+			return false;
+		}		
+	}
+	
+	
+	
 	//USE CASE 1------
 	public String validateName(String s) {
 		String regex = "(^[A-Z]{1}[a-z]{2,}$)";	
